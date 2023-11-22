d962bd42cdc887516312e29d68c2018b5c877d07
MoaadKa <moaadkarkouri@gmail.com>
2023-11-22 23:34:08 +0100

    Changes

--- employeemanager/src/main/java/com/practice/employeemanager/model/EmployeeRepository.java
+++
@@ -1,4 +0,0 @@
-package com.practice.employeemanager.model;
-
-public interface EmployeeRepository {
-}
---
+++ employeemanager/src/main/java/com/practice/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.practice.employeemanager.repository;
+
+public interface EmployeeRepository {
+}
